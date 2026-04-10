78215843c6518a41be3c0a46abdf10fefb5fd370
nafemedicaldevices-cell <nafemedicaldevices@gmail.com>
2026-04-11 01:38:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Overdue Step 1")
+st.title("Overdue Step 2")
 
 overdue = pd.read_excel("Overdue.xlsx")
 
-st.subheader("📄 Raw Data")
+# =========================
+# BASIC CLEANING (Step 2)
+# =========================
+overdue = overdue.iloc[:, :9].copy()
+
+st.subheader("📄 After Selecting 9 Columns")
 st.dataframe(overdue)
