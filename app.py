aa41052515e7e61f7d933fe97afe04209ef0597c
nafemedicaldevices-cell <nafemedicaldevices@gmail.com>
2026-04-10 21:37:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 import streamlit as st
 import data_pipeline as dp
 
-st.title("Dashboard")
-
 data = dp.load_data()
 
-rep_target = dp.build_target_pipeline(
+rep = dp.build_target_pipeline(
     data["target_rep"],
     "Rep Code",
     data["mapping"]
 )
 
-st.write(rep_target["value_full"].head())
+st.write(rep["value_full"])
