02f9647ef514b3b3aa8ad370c49774f6e63d6cb8
nafemedicaldevices-cell <nafemedicaldevices@gmail.com>
2026-04-10 21:21:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,13 @@ data = dp.load_data()
 st.title("Test Load Data")
 
 st.write(data["sales"].head())
+
+
+import pandas as pd
+import data_pipeline as dp
+
+mapping = pd.read_excel("Mapping.xlsx")
+
+rep = dp.build_target_pipeline("Target Rep.xlsx", "Rep Code", mapping)
+
+print(rep["value_full"].head())
