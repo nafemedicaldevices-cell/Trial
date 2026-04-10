5fe332f852e11f6b40b39c5024b2fdb489521a02
nafemedicaldevices-cell <nafemedicaldevices@gmail.com>
2026-04-10 21:34:34 +0200

    Update data_pipeline.py

--- data_pipeline.py
+++ data_pipeline.py
@@ -1,35 +1,7 @@
 import pandas as pd
 
 def load_data():
-    sales = pd.read_excel("Sales.xlsx")
-    overdue = pd.read_excel("Overdue.xlsx")
-    extra_discounts = pd.read_excel("Extradiscounts.xlsx")
-    opening = pd.read_excel("Opening.xlsx")
-    opening_detail = pd.read_excel("Opening Detail.xlsx")
 
-    target_manager = pd.read_excel("Target Manager.xlsx")
-    target_area = pd.read_excel("Target Area.xlsx")
-    target_rep = pd.read_excel("Target Rep.xlsx")
-
-    mapping = pd.read_excel("Mapping.xlsx")
-    codes = pd.read_excel("Code.xlsx")
-
-    return {
-        "sales": sales,
-        "overdue": overdue,
-        "extra_discounts": extra_discounts,
-        "opening": opening,
-        "opening_detail": opening_detail,
-        "target_manager": target_manager,
-        "target_area": target_area,
-        "target_rep": target_rep,
-        "mapping": mapping,
-        "codes": codes
-    }
-
-import pandas as pd
-
-def load_data():
     sales = pd.read_excel("Sales.xlsx")
     overdue = pd.read_excel("Overdue.xlsx")
     extra_discounts = pd.read_excel("Extradiscounts.xlsx")
