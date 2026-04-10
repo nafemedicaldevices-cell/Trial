af0a14babb6ac8e5ad041ad9f466891a28596ee3
nafemedicaldevices-cell <nafemedicaldevices@gmail.com>
2026-04-10 21:32:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
 import streamlit as st
-import data_loader as dl
 import data_pipeline as dp
 
 st.title("Sales Dashboard")
 
-data = dl.load_data()
-
-rep = dp.build_target_pipeline(
-    data["target_rep"],
-    "Rep Code",
-    data["mapping"]
-)
-
-st.write(rep["value_full"].head())
+# مثال تشغيل
+data = dp.load_data()   # لو عندك load_data جوه pipeline
+st.write(data["sales"].head())
