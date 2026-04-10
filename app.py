8f74afde55a7aa7a5a891b2ba373128fce9daba1
nafemedicaldevices-cell <nafemedicaldevices@gmail.com>
2026-04-10 21:19:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,8 @@
 import streamlit as st
-from data_pipeline import run_pipeline
+import data_pipeline as dp
 
-data = run_pipeline()
+data = dp.load_data()
 
-st.set_page_config(page_title="Sales Dashboard", layout="wide")
+st.title("Test Load Data")
 
-st.title("📊 Sales Performance Dashboard")
-
-# =========================
-# SALES
-# =========================
-st.subheader("Sales Data")
-st.dataframe(data["sales"].head())
-
-# =========================
-# OVERDUE
-# =========================
-st.subheader("Overdue Data")
-st.dataframe(data["overdue"].head())
-
-# =========================
-# OPENING DETAIL
-# =========================
-st.subheader("Opening Detail")
-st.dataframe(data["opening_detail"].head())
+st.write(data["sales"].head())
