19dfff2deea15e9108177027d2025377e8d0f58d
nafemedicaldevices-cell <nafemedicaldevices@gmail.com>
2026-04-11 22:17:09 +0200

    Update cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -1,1 +1,15 @@
+import streamlit as st
+import pipelines_test as p
 
+data = p.load_data()
+
+target = p.build_target_pipeline(data["target_rep"], "Rep Code", data["mapping"])
+sales = p.build_sales_pipeline(data["sales"], data["mapping"], data["codes"])
+
+st.title("Test Dashboard")
+
+st.subheader("Target")
+st.dataframe(target["value_table"])
+
+st.subheader("Sales")
+st.dataframe(sales["rep_value"])
