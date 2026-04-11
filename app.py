fcb1f974871dbb698abb25b7858566341e812108
nafemedicaldevices-cell <nafemedicaldevices@gmail.com>
2026-04-11 02:11:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
-# =========================
-# REP EXTRACTION
-# =========================
-opening["Rep Code"] = pd.NA
-opening["Old Rep Name"] = pd.NA
+import streamlit as st
+import pandas as pd
 
-mask = opening["Branch"].astype(str).str.strip().eq("كود المندوب")
-
-opening.loc[mask, "Rep Code"] = opening.loc[mask, "Opening Balance"]
-opening.loc[mask, "Old Rep Name"] = opening.loc[mask, "Total Sales After Invoice Discounts"]
-
-opening[["Rep Code", "Old Rep Name"]] = opening[["Rep Code", "Old Rep Name"]].ffill()
+# LOAD DATA
+opening = pd.read_excel("Opening.xlsx")
+codes = pd.read_excel("Code.xlsx")
