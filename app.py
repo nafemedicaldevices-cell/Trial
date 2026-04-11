67fef8d329e5446fa49916175de3a57ffd1a39b0
nafemedicaldevices-cell <nafemedicaldevices@gmail.com>
2026-04-11 02:11:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ import pandas as pd
 # LOAD DATA
 opening = pd.read_excel("Opening.xlsx")
 codes = pd.read_excel("Code.xlsx")
+st.title("Opening Dashboard")
+
+st.write("Data Preview 👇")
+st.dataframe(opening.head())
