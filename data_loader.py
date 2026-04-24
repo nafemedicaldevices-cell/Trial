cb90eb952ab04cd6a6c46af59b4d20a53c430ee9
nafemedicaldevices-cell <nafemedicaldevices@gmail.com>
2026-04-25 02:33:33 +0300

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,14 +1,1 @@
-import pandas as pd
 
-def load_all():
-
-    sales = pd.read_excel("Sales.xlsx")
-    codes = pd.read_excel("Code.xlsx")
-
-    sales.columns = sales.columns.str.strip()
-    codes.columns = codes.columns.str.strip()
-
-    # 🔥 بس نعمل merge بدون لمس أعمدة مش موجودة
-    sales = sales.merge(codes, on="Rep Code", how="left")
-
-    return {"sales": sales}
