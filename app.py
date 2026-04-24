f4548ad1d4bf1345cf15b7e0019c1d4bead88b4d
nafemedicaldevices-cell <nafemedicaldevices@gmail.com>
2026-04-25 01:48:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,18 @@
 import streamlit as st
-
-from cleaning import (
-    load_client_haraka,
-    load_targets,
-    load_haraka,
-    load_overdue
-)
+from cleaning import load_client_haraka, load_targets, load_haraka, load_overdue
 
 st.set_page_config(page_title="Dashboard", layout="wide")
 st.title("📊 Sales Dashboard")
 
-# =========================
-# LOAD DATA
-# =========================
 client = load_client_haraka()
 targets = load_targets()
 haraka = load_haraka()
 overdue = load_overdue()
 
-# =========================
-# TABS
-# =========================
-tab1, tab2, tab3, tab4 = st.tabs([
-    "Client",
-    "Targets",
-    "Haraka",
-    "Overdue"
-])
+tab1, tab2, tab3, tab4 = st.tabs(["Client","Targets","Haraka","Overdue"])
 
 with tab1:
     st.dataframe(client, use_container_width=True)
-    st.metric("Sales", f"{client['Sales Value'].sum():,.0f}")
 
 with tab2:
     st.dataframe(targets, use_container_width=True)
